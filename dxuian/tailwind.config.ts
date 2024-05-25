a9cf368f7fcb854224c5d5628aeda6bc150fb609
dxuian <deathxunnat@gmail.com>
2024-05-26 01:56:55 +0530

    addblog ok blog ok main page ok but smthn missing and db ok

--- dxuian/tailwind.config.ts
+++ dxuian/tailwind.config.ts
@@ -1,4 +1,8 @@
 import type { Config } from "tailwindcss"
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 
 const config = {
   darkMode: ["class"],
@@ -7,6 +11,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    "./node_modules/flowbite/**/*.js",
 	],
   prefix: "",
   theme: {
@@ -72,9 +77,30 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("rippleui"),
+    require("tailwindcss"),
+    require('flowbite/plugin'),
+    require("autoprefixer"),
+    addVariablesForColors
+  ],
 } satisfies Config
 
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config
