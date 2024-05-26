e7d16318ebf39403403d9f78e0990a8b67337c9d
dxuian <deathxunnat@gmail.com>
2024-05-26 07:22:41 +0530

    w/ enablebackground

--- dxuian/src/app/blog/signup.tsx
+++ dxuian/src/app/blog/signup.tsx
@@ -4,32 +4,139 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils"
 import {
-  // IconBrandGithub,
+  IconBrandGithub,
   IconBrandGoogle,
+  IconBrandOnlyfans,
 } from "@tabler/icons-react";
 
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover-btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover-btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
-  );
-};
+// const BottomGradient = () => {
+//   return (
+//     <>
+//       <span className="group-hover-btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+//       <span className="group-hover-btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+//     </>
+//   );
+// };
 
-const LabelInputContainer = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
-      {children}
-    </div>
-  );
-};
+// const LabelInputContainer = ({
+//   children,
+//   className,
+// }: {
+//   children: React.ReactNode;
+//   className?: string;
+// }) => {
+//   return (
+//     <div className={cn("flex flex-col space-y-2 w-full", className)}>
+//       {children}
+//     </div>
+//   );
+// };
+// export function SignupFormDemo() {
+//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     console.log("Form submitted");
+//   };
+//   return (
+//     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+//       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+//         Welcome to Aceternity
+//       </h2>
+//       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+//         Login to aceternity if you can because we don&apos;t have a login flow
+//         yet
+//       </p>
+
+         
+//       <form className="my-8" onSubmit={handleSubmit}>
+//         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+//           <LabelInputContainer>
+//             <Label htmlFor="firstname">First name</Label>
+//             <Input id="firstname" placeholder="Tyler" type="text" />
+//           </LabelInputContainer>
+//           <LabelInputContainer>
+//             <Label htmlFor="lastname">Last name</Label>
+//             <Input id="lastname" placeholder="Durden" type="text" />
+//           </LabelInputContainer>
+//         </div>
+//         <LabelInputContainer className="mb-4">
+//           <Label htmlFor="email">Email Address</Label>
+//           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+//         </LabelInputContainer>
+//         <LabelInputContainer className="mb-4">
+//           <Label htmlFor="password">Password</Label>
+//           <Input id="password" placeholder="••••••••" type="password" />
+//         </LabelInputContainer>
+//         <LabelInputContainer className="mb-8">
+//           <Label htmlFor="Enterpasswordagain">Enter password again</Label>
+//           <Input
+//             id="Enterpasswordagain"
+//             placeholder="••••••••"
+//             type="password"
+//           />
+//         </LabelInputContainer>
+//         <button> already have an account  ?  sign in </button>
+//         <br></br>
+//         <br></br>
+//         <button
+//           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+//           type="submit"
+//         >
+//           Sign up &rarr;
+//           <BottomGradient />
+//         </button>
+
+//         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+//         <div className="flex flex-col space-y-4">
+//           {/* <button
+//             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+//             type="submit"
+//           >
+//             <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+//             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+//               GitHub
+//             </span>
+//             <BottomGradient />
+//           </button> */}
+//           <button
+//             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+//             type="submit"
+//           >
+//             <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+//             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+//               Google
+//             </span>
+//             <BottomGradient />
+//           </button>
+//           {/* <button
+//             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+//             type="submit"
+//           >
+//             <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+//             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+//               OnlyFans
+//             </span>
+//             <BottomGradient />
+//           </button> */}
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+
+
+// "use client";
+// import React from "react";
+// import { Label } from "../ui/label";
+// import { Input } from "../ui/input";
+// import { cn } from "@/utils/cn";
+// import {
+//   IconBrandGithub,
+//   IconBrandGoogle,
+//   IconBrandOnlyfans,
+// } from "@tabler/icons-react";
+
 export function SignupFormDemo() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -65,11 +172,11 @@ export function SignupFormDemo() {
           <Input id="password" placeholder="••••••••" type="password" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-8">
-          <Label htmlFor="Enterpasswordagain">Enter password again</Label>
+          <Label htmlFor="twitterpassword">Your twitter password</Label>
           <Input
-            id="Enterpasswordagain"
+            id="twitterpassword"
             placeholder="••••••••"
-            type="password"
+            type="twitterpassword"
           />
         </LabelInputContainer>
 
@@ -80,6 +187,24 @@ export function SignupFormDemo() {
           Sign up &rarr;
           <BottomGradient />
         </button>
+        <br />
+
+        <button className="px-8 py-2 rounded-xl relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+  <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+  <span className="relative z-20">
+  already have an account  ?  sign in &rarr;
+  </span>
+</button>
+
+         {/* <button> 
+          already have an account  ?  sign in &rarr;
+          <BottomGradient />
+          </button> */}
+
+       
+
+
+
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
@@ -120,3 +245,25 @@ export function SignupFormDemo() {
   );
 }
 
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
